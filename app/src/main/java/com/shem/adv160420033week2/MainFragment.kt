7f5979ac84b93e06383dff89ec30eccd8eaea290
blixondi <101459072+blixondi@users.noreply.github.com>
2023-03-08 18:46:16 +0700

    Added stuff on game_navigation.xml, GameFragment.kt, MainActivity.kt, MainFragment.kt

--- app/src/main/java/com/shem/adv160420033week2/MainFragment.kt
+++ app/src/main/java/com/shem/adv160420033week2/MainFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import com.google.android.material.textfield.TextInputEditText
 
 
 class MainFragment : Fragment() {
@@ -15,6 +18,15 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnStart = view.findViewById<Button>(R.id.btnStart)
+        btnStart.setOnClickListener{
+            val action = MainFragmentDirections.actionGameFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 }
