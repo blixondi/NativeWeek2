2a87eceea1e0ef8054a46eb3f0e7dfa528d0b782
blixondi <blixondi@gmail.com>
2023-03-15 14:17:09 +0700

    remove some unnecessary TODOs

--- app/src/main/java/com/shem/adv160420033week2/OptionFragment.kt
+++ app/src/main/java/com/shem/adv160420033week2/OptionFragment.kt
@@ -8,18 +8,18 @@ import android.view.ViewGroup
 
 
 
+class OptionFragment : Fragment() {
 
-class HistoryFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        return inflater.inflate(R.layout.fragment_option, container, false)
     }
 
 
